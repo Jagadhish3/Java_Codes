1f7a29b88a875ce1298cb9cebca0bba478e09144
Sriram Akula <sriramakula212@gmail.com>
2024-01-22 20:01:56 +0530

    Added

--- Main_Java/Operators/Intro.java
+++ Main_Java/Operators/Intro.java
@@ -1,20 +1,35 @@
 class Intro{
     public static void main(String args[]){
-        int a=2;
-        int b=5;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
-        System.out.println(a+1);
+        // int a=2;
+        // int b=5;
+        // System.out.println(a+b);
+        // System.out.println(a-b);
+        // System.out.println(a*b);
+        // System.out.println(a/b);
+        // System.out.println(a%b);
+        // System.out.println(a+1);
         
-        int output=b++;
-        System.out.println(output);
-        System.out.println(b); //post increment ++
+        // int output=b++;
+        // System.out.println(output);
+        // System.out.println(b); //post increment ++
 
-        int output1=++a;
-        System.out.println(output1);//pre increment ++
+        // int output1=++a;
+        // System.out.println(output1);//pre increment ++
         
+
+
+        //Comparison Operators
+        // <   >  <=  >=   ==  !=
+        // int num1=20;
+        // int num2=40;
+        // System.out.println(num1>num2); //returns boolean value
+
+
+        //Logical Operators
+        //   &&  ||  !
+        int x=6,y=8,z=10;
+
+        System.out.println(x<y && x<z);
+
     }
 }
