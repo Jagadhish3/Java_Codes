8c48a20e88eeccac8cadaa18ce8b3475fe16f6d8
Sriram Akula <sriramakula212@gmail.com>
2024-01-22 19:43:39 +0530

    increment operator

--- Main_Java/Operators/Intro.java
+++ Main_Java/Operators/Intro.java
@@ -1,11 +1,20 @@
 class Intro{
     public static void main(String args[]){
         int a=2;
-        int b=3;
+        int b=5;
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
         System.out.println(a%b);
+        System.out.println(a+1);
+        
+        int output=b++;
+        System.out.println(output);
+        System.out.println(b); //post increment ++
+
+        int output1=++a;
+        System.out.println(output1);//pre increment ++
+        
     }
 }
