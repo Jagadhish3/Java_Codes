54ac3526c85ef41cb17de182149d17dd11734648
Sriram Akula <sriramakula212@gmail.com>
2024-01-23 19:53:33 +0530

    Switch_Case

--- Main_Java/05_Conditional St/If_Else.java
+++ Main_Java/05_Conditional St/If_Else.java
@@ -1,17 +1,21 @@
 class If_Else{
     public static void main(String args[]){
-        int num1=15;
-        int num2=20;
-        if(num1<10 && num2>15){
-            System.out.println("Inside If");
-        }
-        else if(num2<20 || num1>10){
-            System.out.println("Inside Else If");
-        }
-        else{
-            System.out.println("Inside Else");
-        }
-        
+        // int num1=15;
+        // int num2=20;
+        // if(num1<10 && num2>15){
+        //     System.out.println("Inside If");
+        // }
+        // else if(num2<20 || num1>10){
+        //     System.out.println("Inside Else If");
+        // }
+        // else{
+            //     System.out.println("Inside Else");
+            // }
+            
+            
+            int num=4;
+            String output =num % 2 == 0 ? "Even":"Odd";  //Terenary Operator
+            System.out.println(output);
         
     }
 }
---
+++ Main_Java/05_Conditional St/Switch_Case.java
@@ -0,0 +1,54 @@
+class Switch_Case {
+    public static void main(String[] args) {
+        // int n=5;
+
+        // switch(n){
+        //     case 1:
+        //     System.out.println("Monday");
+        //     break;
+        //     case 2:
+        //     System.out.println("Tuesday");
+        //     break;
+        //     case 3:
+        //     System.out.println("Wednesday");
+        //     break;
+        //     case 4:
+        //     System.out.println("Thursday");
+        //     break;
+        //     case 5:
+        //     System.out.println("Friday");
+        //     break;
+        //     case 6:
+        //     System.out.println("Saturday");
+        //     break;
+        //     default:
+        //     System.out.println("Sunday");
+        //     break;
+
+        // }
+
+        // String day = "Tuesday";
+        // String output="";
+        // switch(day){
+        //     case "Monday","Tuesday":
+        //         //System.out.println("Class From 7 to 9 PM");
+        //         output="Class From 7 to 9 PM";
+        //         break;
+        //     case "Wednesday":
+        //         System.out.println("Class From 7 to 8 PM");
+        //         break;
+        //     default:
+        //         System.out.println("No Class");
+        //         break;
+        //     }
+        //     System.out.println(output);
+        String day = "Sunday";
+        String output="";
+        switch(day){
+            case "Monday","Tuesday" -> output="Class From 7 to 9 PM";
+            case "Wednesday" -> System.out.println("Class From 7 to 8 PM");
+            default -> System.out.println("No Class");
+         } //No use of break statements
+        //System.out.println(output);
+    }
+}
