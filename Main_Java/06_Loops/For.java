8a9fba5ed8d8f730f4f85852b29b3558c6d4c697
Sriram Akula <sriramakula212@gmail.com>
2024-01-24 19:24:02 +0530

    changes

--- Main_Java/06_Loops/For.java
+++ Main_Java/06_Loops/For.java
@@ -1,7 +1,14 @@
 class For{
     public static void main(String args[]){
-        for(int i=0;i<5;i++){
-            System.out.println("Hello World "+i);
+        // for(int i=0;i<5;i++){
+        //     System.out.println("Hello World "+i);
+        // }
+        for(int i=0;i<4;i++){
+            System.out.print("* ");
+            for(int j=0;j<3;j++){
+                System.out.print("* ");
+            }
+            System.out.println("\n");
         }
     }
 }
