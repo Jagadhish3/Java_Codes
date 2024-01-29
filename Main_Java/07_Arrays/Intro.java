d326987cf291c4a863aad2b0a07ed201c0873de5
Sriram Akula <sriramakula212@gmail.com>
2024-01-29 19:45:15 +0530

    array

---
+++ Main_Java/07_Arrays/Intro.java
@@ -0,0 +1,14 @@
+class Intro{
+    public static void main(String args[]){
+        //Arrays
+        int arr[]={1,2,3,4,5};
+        System.out.println(arr[3]);
+    }
+}
+
+/*Fast access -> It is very fat to access in any 
+ele in an array(by index),accessing the first ele 
+will take same time as lat ele*/
+
+//Fixed size -> once created, we can't change size
+
