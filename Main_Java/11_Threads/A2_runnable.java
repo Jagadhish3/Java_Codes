e7499af6885719031867cbf75a8c0ad99b0bbea8
Sriram Akula <sriramakula212@gmail.com>
2024-03-20 19:53:52 +0530

    20/03

--- Main_Java/11_Threads/A2_runnable.java
+++ Main_Java/11_Threads/A2_runnable.java
@@ -1,6 +1,36 @@
-class A implements Runnable{
-    public void run(){
-        for(int i=0;i<=15;i++){
+// class A implements Runnable{
+    // public void run(){
+    //     for(int i=0;i<=15;i++){
+            
+    //         System.out.println("Executing A");
+    //         try {
+    //             Thread.sleep(10);
+    //         } catch (InterruptedException e) {
+                
+    //             e.printStackTrace();
+    //         }
+    //     }
+    // }
+// }
+// class B implements Runnable{
+    // public void run(){
+    //     for(int i=0;i<=15;i++){
+            
+    //         System.out.println("From B");
+    //         try {
+    //             Thread.sleep(10);
+    //         } catch (InterruptedException e) {
+                
+    //             e.printStackTrace();
+    //         }
+    //     }
+    // }
+// }
+public class A2_runnable {
+    public static void main(String[] args) {
+        Runnable obj1 = () -> {
+            
+        for(int i=0;i<=5;i++){
             
             System.out.println("Executing A");
             try {
@@ -10,11 +40,11 @@ class A implements Runnable{
                 e.printStackTrace();
             }
         }
-    }
-}
-class B implements Runnable{
-    public void run(){
-        for(int i=0;i<=15;i++){
+    
+        };
+        Runnable obj2 = new Runnable() {
+            public void run(){
+        for(int i=0;i<=5;i++){
             
             System.out.println("From B");
             try {
@@ -25,11 +55,7 @@ class B implements Runnable{
             }
         }
     }
-}
-public class A2_runnable {
-    public static void main(String[] args) {
-        Runnable obj1 = new A();
-        Runnable obj2 = new B();
+        };
 
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
